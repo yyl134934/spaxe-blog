2716ad6b63c72ab49911becfe8ea6348a4a894c7
yaoc <yyl134934@163.com>
2023-07-19 23:36:01 +0800

    feat(blog):访问文章不存在时拦截

---
+++ src/app/blog/[slug]/not-found.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+function NotFound() {
+  return (
+    <div className='flex justify-center items-center flex-col'>
+      <Image priority src={'/404.svg'} alt='404|not found' height={600} width={600} />
+      <p className=''>OOPS！页面暂未施工😛</p>
+    </div>
+  );
+}
+export default NotFound;
