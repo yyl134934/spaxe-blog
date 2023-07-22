db054fd67e6ce3e81800238b2f285f095a10122b
yaoc <yyl134934@163.com>
2023-07-22 21:22:59 +0800

    perf(query):优化查询 - 优化接口定义 - 优化分页查询

--- src/entity/Response.d.ts
+++ src/entity/Response.d.ts
@@ -1,1 +1,1 @@
-export type ResponseData<T> = { code: number | string; ret_msg: string; data: Partial<T>; [key?: string]: any };
+export type ResponseData<T> = { code: number | string; ret_msg: string; data: Partial<T>[]; [key?: string]: any };
--- src/layout/SearchLayout/SearchLayout.tsx
+++ src/layout/SearchLayout/SearchLayout.tsx
@@ -1,76 +1,28 @@
 'use client';
-import useSWRInfinite from 'swr/infinite';
 import PostLayout from '@/layout/PostLayout';
 import { Post } from '@/entity/Common';
-import { useSearch } from './hooks';
-import { ResponseData } from '@/entity/Response';
+import { usePostsQuery, useSearch } from './hooks';
 import dynamic from 'next/dynamic';
 
 const MoreLoad = dynamic(() => import('@/components/MoreLoad'), { ssr: false });
 
-type SearchLayoutProps = {
+interface SearchLayoutProps {
   children?: React.ReactNode;
   pageProps?: {
     title?: string;
     path?: string;
-    initialKeyword?: string;
+    tag?: string;
   };
-};
-
-const getSearch = (obj: Record<string, any>) => {
-  const isUseful = (value: any) => !(value === null || value === undefined || value === '');
-  const query = Object.entries(obj)
-    .filter(([_, value]) => isUseful(value))
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
-
-  return `?${query}`;
-};
-
-const hasNextPage = (size: number, data: ResponseData<Post>[], offset = 5) => {
-  const lastPage = data[data.length - 1] ?? [];
-  return lastPage.data?.length < offset || size > data.length;
-};
-
-/**
- *  一个函数，拿到每个页面的 key，
- * `fetcher` 接受它的返回值。
- * 如果返回是 `null`，该页面不会开始请求。
- * @param searchKeyword
- * @returns
- */
-const getKey = (searchKeyword: string) => (pageIndex: number, previousPageData: ResponseData<Post>) => {
-  const offset = '5';
-  if (pageIndex !== 0 && (!previousPageData || previousPageData.data?.length < offset)) {
-    return null;
-  }
-  const search = getSearch({ search: searchKeyword, page: pageIndex + 1, offset });
-  return `${search}`;
-};
-
-const fetcher = async (search: string): Promise<ResponseData<Post>> => {
-  const { host, protocol } = window?.location;
-  const pathname = 'api/getPostData';
-  const res = await fetch(`${protocol}//${host}/${pathname}${search}`);
-  return res.json();
-};
+}
 
 export default function SearchLayout({ children, pageProps }: SearchLayoutProps) {
-  const { initialKeyword: initial, ...rest } = pageProps ?? { initialKeyword: '' };
-  const { keyword: searchKeyword, onSearch } = useSearch(initial);
+  const { tag, ...rest } = pageProps ?? { initialKeyword: '' };
+  const { keyword: searchKeyword, onSearch } = useSearch();
 
-  const options = {
-    initialSize: 1, //最初应加载的页面数量
-    revalidateAll: false, //始终尝试重新验证所有页面
-    revalidateFirstPage: false, //始终尝试重新验证第一页
-    persistSize: false, //当第一页的 key 发生变化时，不将 page size（或者 initialSize 如果设置了该参数）重置为 1
-  };
-  const { data = [], size, setSize } = useSWRInfinite(getKey(searchKeyword), fetcher, options);
-  const summaryData = data.reduce((acc, cur) => acc.concat(cur.data as Post), [] as Post[]);
-  const isEnd = hasNextPage(size, data);
+  const { summaryData, setSize, isEnd } = usePostsQuery({ search: searchKeyword, tag });
 
   const handleNext = () => {
-    setSize(size + 1);
+    setSize((prevSize) => prevSize + 1);
   };
 
   return (
